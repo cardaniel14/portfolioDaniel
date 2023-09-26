878000c480aaaddcf466eb8d98730b707ed5529f
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2023-09-27 00:42:34 +0100

    Creating repository for bootstrap project

---
+++ script.js
@@ -0,0 +1,7 @@
+// window.onload = function () {
+//   // Obtener el elemento HR
+//   const hrElement = document.querySelector(".animated-hr");
+
+//   // Expandir al 100% de ancho
+//   hrElement.style.width = "100%";
+// };
