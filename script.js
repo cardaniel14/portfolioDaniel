4146e4331face7ec566157db1decd95c56f2b0b9
Carlos Daniel Erazo <carlosderazo.07@gmail.com>
2024-01-04 15:54:23 +0000

    adicion de typed en proyecto

--- script.js
+++ script.js
@@ -1,10 +1,26 @@
 //Uso de typed framework para hacer la letra animada
 
 const typed = new Typed(".typed", {
-	strings: ["Front-End Developer"],
-	typeSpeed: 100,
-	
-	loop: false
+  strings: ["Front-End Developer"],
+  typeSpeed: 100,
+  cursorBlinking: false,
+  loop: false,
 });
 
-console.log(typed)
+typed.cursor.innerText = "";
+
+function showScroll() {
+  let animation = document.querySelectorAll(".animation");
+  //   let offsetTop = document.querySelector(".animation").offsetTop;
+  let scroll = document.documentElement.scrollTop;
+
+  for (let i = 0; i < animation.length; i++) {
+    let offsetTop = animation[i].offsetTop;
+    if (scroll > offsetTop - 500) {
+      animation[i].style.opacity = 1;
+      animation[i].classList.add("show");
+    }
+  }
+}
+
+window.addEventListener("scroll", showScroll);
